9692f031e155ef99e780c907ae39b25b1466b8c8
User Namovich <obletov>
2023-08-23 18:08:31 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://9a463a30-8de5-42f7-a606-9076f883bea0.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
